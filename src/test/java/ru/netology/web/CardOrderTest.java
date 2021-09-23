6680c9658c9c7316d93d921969f3261dd35fdf0b
Rostiks52 <ksyu_05@mail.ru>
2021-09-23 19:23:14 +0300

    Dependency added

--- src/test/java/ru/netology/web/CardOrderTest.java
+++ src/test/java/ru/netology/web/CardOrderTest.java
@@ -9,7 +9,6 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
-import io.github.bonigarcia.wdm.WebDriverManager;
 
 
 import static org.junit.jupiter.api.Assertions.assertEquals;

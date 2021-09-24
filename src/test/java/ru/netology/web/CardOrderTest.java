82b9d97dad651e008ae1d8f4f31253f3e150426d
Rostiks52 <ksyu_05@mail.ru>
2021-09-24 21:48:26 +0300

    Tests added

--- src/test/java/ru/netology/web/CardOrderTest.java
+++ src/test/java/ru/netology/web/CardOrderTest.java
@@ -14,17 +14,17 @@ import org.openqa.selenium.chrome.ChromeOptions;
 
 import static java.lang.System.*;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.openqa.selenium.By.cssSelector;
 
 import java.util.List;
 
- class CardOrderTest {
+class CardOrderTest {
 
     private WebDriver driver;
 
     @BeforeAll
     static void setUpAll() {
         WebDriverManager.chromedriver().setup();
-//        setProperty("webdriver.chrome.driver", "./driver/win/chromedriver.exe");
     }
 
     @BeforeEach
@@ -42,16 +42,74 @@ import java.util.List;
         driver = null;
     }
 
-        @Test
-        void shouldTestV1() {
-            driver.get("http://localhost:9999");
-            List<WebElement> elements = driver.findElements(By.className("input__control"));
-            elements.get(0).sendKeys("Владимир Ленин");
-            elements.get(1).sendKeys("+79221234567");
-            driver.findElement(By.className("checkbox__box")).click();
-            driver.findElement(By.className("button")).click();
-            String text = driver.findElement(By.tagName("p")).getText();
-            assertEquals("Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время.", text.trim());
+    @Test
+    void shouldReturnHappyPath() {
+        driver.get("http://localhost:9999");
+        List<WebElement> elements = driver.findElements(By.className("input__control"));
+        elements.get(0).sendKeys("Владимир Ленин");
+        elements.get(1).sendKeys("+79221234567");
+        driver.findElement(By.className("checkbox__box")).click();
+        driver.findElement(By.className("button")).click();
+        String text = driver.findElement(By.tagName("p")).getText();
+        assertEquals("Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время.", text.trim());
 
-        }
+    }
+
+    @Test
+    void shouldValidateIfAllFieldsAreEmpty() {
+        driver.get("http://localhost:9999");
+        driver.findElement(cssSelector("[data-test-id=name] input")).sendKeys("");
+        driver.findElement(cssSelector("[data-test-id=phone] input")).sendKeys("");
+        driver.findElement(cssSelector(".checkbox__box")).click();
+        driver.findElement(cssSelector("button")).click();
+        String text1 = driver.findElement(cssSelector("[data-test-id=name] [class=input__sub]")).getText();
+        assertEquals("Поле обязательно для заполнения", text1.trim());
+        String text2 = driver.findElement(cssSelector("[data-test-id=phone] [class=input__sub]")).getText();
+        assertEquals("На указанный номер моб. тел. будет отправлен смс-код для подтверждения заявки на карту. Проверьте, что номер ваш и введен корректно.", text2.trim());
+    }
+
+    @Test
+    void shouldNoticeIfFieldNameIsEmpty() {
+        driver.get("http://localhost:9999");
+        driver.findElement(cssSelector("[data-test-id=name] input")).sendKeys("");
+        driver.findElement(cssSelector("[data-test-id=phone] input")).sendKeys("+79221234567");
+        driver.findElement(cssSelector(".checkbox__box")).click();
+        driver.findElement(cssSelector("button")).click();
+        String text = driver.findElement(cssSelector("[data-test-id=name] [class=input__sub]")).getText();
+        assertEquals("Поле обязательно для заполнения", text.trim());
+    }
+
+    @Test
+    void shouldNoticeIfFieldTelIsEmpty() {
+        driver.get("http://localhost:9999");
+        driver.findElement(cssSelector("[data-test-id=name] input")).sendKeys("Владимир Ленин");
+        driver.findElement(cssSelector("[data-test-id=phone] input")).sendKeys("");
+        driver.findElement(cssSelector(".checkbox__box")).click();
+        driver.findElement(cssSelector("button")).click();
+        String text = driver.findElement(cssSelector("[data-test-id=phone] [class=input__sub]")).getText();
+        assertEquals("Поле обязательно для заполнения", text.trim());
+    }
+
+    @Test
+    void shouldNoticeIfFieldNameContainsLatinSymbols() {
+        driver.get("http://localhost:9999");
+        driver.findElement(cssSelector("[data-test-id=name] input")).sendKeys("Vladimir Lenin");
+        driver.findElement(cssSelector("[data-test-id=phone] input")).sendKeys("+79221234567");
+        driver.findElement(cssSelector(".checkbox__box")).click();
+        driver.findElement(cssSelector("button")).click();
+        String text = driver.findElement(cssSelector("[data-test-id=name] [class=input__sub]")).getText();
+        assertEquals("Имя и Фамилия указаные неверно. Допустимы только русские буквы, пробелы и дефисы.", text.trim());
+    }
+
+    @Test
+    void shouldNoticeIfFieldNameContainsDigits() {
+        driver.get("http://localhost:9999");
+        driver.findElement(cssSelector("[data-test-id=name] input")).sendKeys("9876541");
+        driver.findElement(cssSelector("[data-test-id=phone] input")).sendKeys("+79221234567");
+        driver.findElement(cssSelector(".checkbox__box")).click();
+        driver.findElement(cssSelector("button")).click();
+        String text = driver.findElement(cssSelector("[data-test-id=name] [class=input__sub]")).getText();
+        assertEquals("Имя и Фамилия указаные неверно. Допустимы только русские буквы, пробелы и дефисы.", text.trim());
+    }
+
 }
